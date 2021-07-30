b802a70afe123af3114f9c2cec1214d431509c20
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-07-30 21:11:34 +0400

    comment deleted

--- src/MonthlyPaymentService.java
+++ src/MonthlyPaymentService.java
@@ -1,8 +1,7 @@
 public class MonthlyPaymentService {
     public double calculate(double loanAmount, double interest, double months) {
-        // А=К*(П/(1+П)-М-1), где К – сумма кредита, П – процентная ставка, М – количество месяцев
-        double monthlyRate = interest / 12/100;
-        double annuityCoefficient = monthlyRate * (Math.pow(1 + monthlyRate,months)) / ((Math.pow(1 + monthlyRate,months)) - 1);
+        double monthlyRate = interest / 12 / 100;
+        double annuityCoefficient = monthlyRate * (Math.pow(1 + monthlyRate, months)) / ((Math.pow(1 + monthlyRate, months)) - 1);
         double payment = loanAmount * annuityCoefficient;
         return payment;
 
